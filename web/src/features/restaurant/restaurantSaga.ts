a0d6c48b4b69fd0abe1dc9b8542a3e009bb29215
Aansil <aansil21@gmail.com>
2026-03-16 02:47:40 +0530

    muted sagas

--- web/src/features/restaurant/restaurantSaga.ts
+++ web/src/features/restaurant/restaurantSaga.ts
@@ -1,21 +1,21 @@
-import { takeLatest, call, put } from "redux-saga/effects";
-import {
-  fetchRestaurantsRequest,
-  fetchRestaurantsSuccess,
-  fetchRestaurantsFailure,
-} from "./restaurantSlice";
-import { restaurants } from "@/data/dummyData";
-import { fakeFetch } from "@/api/fakeApi";
+// import { takeLatest, call, put } from "redux-saga/effects";
+// import {
+//   fetchRestaurantsRequest,
+//   fetchRestaurantsSuccess,
+//   fetchRestaurantsFailure,
+// } from "./restaurantSlice";
+// import { restaurants } from "@/data/dummyData";
+// import { fakeFetch } from "@/api/fakeApi";
 
-function* handleFetchRestaurants() {
-  try {
-    const data = yield call(fakeFetch, restaurants);
-    yield put(fetchRestaurantsSuccess(data));
-  } catch {
-    yield put(fetchRestaurantsFailure("Failed to fetch restaurants"));
-  }
-}
+// function* handleFetchRestaurants() {
+//   try {
+//     const data = yield call(fakeFetch, restaurants);
+//     yield put(fetchRestaurantsSuccess(data));
+//   } catch {
+//     yield put(fetchRestaurantsFailure("Failed to fetch restaurants"));
+//   }
+// }
 
-export function* watchRestaurantSaga() {
-  yield takeLatest(fetchRestaurantsRequest.type, handleFetchRestaurants);
-}
+// export function* watchRestaurantSaga() {
+//   yield takeLatest(fetchRestaurantsRequest.type, handleFetchRestaurants);
+// }
