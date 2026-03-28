b3cd76d348689a3aa8a37d280bfa055c4122515a
Aansil <aansil21@gmail.com>
2026-03-29 03:13:16 +0530

    added @ path alias configuration

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -8,6 +8,15 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      '@features': path.resolve(__dirname, './src/features'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@shared': path.resolve(__dirname, './src/shared'),
+      '@api': path.resolve(__dirname, './src/api'),
+      '@app': path.resolve(__dirname, './src/app'),
+      '@core': path.resolve(__dirname, './src/core'),
+      '@services': path.resolve(__dirname, './src/services'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@contexts': path.resolve(__dirname, './src/contexts'),
     },
   },
 })
