afe7adac48a34b6f7f50447dc51d6a35bb276ad6
Aansil <aansil21@gmail.com>
2026-04-12 20:33:33 +0530

    made debounce timer for dev console mofal for ignoring double click now set to open Logger Console Modal

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,37 +1,52 @@
 import React from "react";
-import {AuthProvider} from './contexts/AuthContext';
+import { AuthProvider } from "./contexts/AuthContext";
 import AppRoutes from "./app/routes";
-import FloatingDevConsole from "@core/dev/ui/components/FloatingDevConsole";
+import FloatingDevConsole from "@/core/dev/ui/modals/FloatingDevConsole";
 // import { ErrorBoundary } from "./shared/components/ErrorBoundary";
-import { CUISINES } from '@core/constants/food';
+import { CUISINES } from "@core/constants/food";
 import { selectAllRestaurants } from "./features/restaurant/restaurantSlice";
 import { selectFeaturedRestaurants } from "./features/restaurant/restaurantSlice";
 import { selectRestaurantLoading } from "./features/restaurant/restaurantSlice";
 import { useAppSelector } from "@app/store/hooks";
+//context
 import { useDevContext } from "@core/dev/contexts/DevContext";
-
-const App:React.FC =()=>{
-    const allRestaurants = useAppSelector(selectAllRestaurants);
-    const featuredRestaurants = useAppSelector(selectFeaturedRestaurants);
-    const loading = useAppSelector(selectRestaurantLoading);
-    const { availableVersions, selectedVersions } = useDevContext();
+import { useLogger } from "./core/dev/logger";
+//dev-modals
+import LogConsole from "./core/dev/logger";
+import { APP_CONFIG } from "./core/config/app.config";
+const App: React.FC = () => {
+  const allRestaurants = useAppSelector(selectAllRestaurants);
+  const featuredRestaurants = useAppSelector(selectFeaturedRestaurants);
+  const loading = useAppSelector(selectRestaurantLoading);
+  const { availableVersions, selectedVersions } = useDevContext();
+  const { open: LogConsoleOpen, setOpen: setLogConsoleOpen } = useLogger();
   // console.log('%c<App/>','color:orange')
   return (
     // <ErrorBoundary>
-      <AuthProvider>
-        <AppRoutes/>
-        {/* Floating Dev Console */}
-            <FloatingDevConsole
-              allRestaurants={allRestaurants}
-              featuredRestaurants={featuredRestaurants}
-              loading={loading}
-              availableVersions={availableVersions}
-              selectedVersions={selectedVersions}
-              cuisineLength={CUISINES.length}
-            />
-      </AuthProvider>
+    <AuthProvider>
+      <AppRoutes />
+      {/* Floating Dev Console */}
+      {APP_CONFIG.DEV_BYPASS_AUTH && (
+        <FloatingDevConsole
+          allRestaurants={allRestaurants}
+          featuredRestaurants={featuredRestaurants}
+          loading={loading}
+          availableVersions={availableVersions}
+          selectedVersions={selectedVersions}
+          cuisineLength={CUISINES.length}
+        />
+      )}
+      {/* Logger-Console    */}
+      <LogConsole
+        open={LogConsoleOpen}
+        onClose={() => {
+          setLogConsoleOpen(false);
+        }}
+      />
+    </AuthProvider>
+
     // </ErrorBoundary>
-  )
-}
+  );
+};
 
 export default App;
