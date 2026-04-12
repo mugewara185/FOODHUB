08269b61252dd664a5716a7d5a303a05db0cebb0
Aansil <aansil21@gmail.com>
2026-04-12 14:15:13 +0530

    moved dev console float icon to overall app from home

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,36 @@
 import React from "react";
 import {AuthProvider} from './contexts/AuthContext';
 import AppRoutes from "./app/routes";
+import FloatingDevConsole from "@core/dev/ui/components/FloatingDevConsole";
+// import { ErrorBoundary } from "./shared/components/ErrorBoundary";
+import { CUISINES } from '@core/constants/food';
+import { selectAllRestaurants } from "./features/restaurant/restaurantSlice";
+import { selectFeaturedRestaurants } from "./features/restaurant/restaurantSlice";
+import { selectRestaurantLoading } from "./features/restaurant/restaurantSlice";
+import { useAppSelector } from "@app/store/hooks";
+import { useDevContext } from "@core/dev/contexts/DevContext";
 
 const App:React.FC =()=>{
+    const allRestaurants = useAppSelector(selectAllRestaurants);
+    const featuredRestaurants = useAppSelector(selectFeaturedRestaurants);
+    const loading = useAppSelector(selectRestaurantLoading);
+    const { availableVersions, selectedVersions } = useDevContext();
   // console.log('%c<App/>','color:orange')
   return (
-    <AuthProvider>
-      <AppRoutes/>
-    </AuthProvider>
+    // <ErrorBoundary>
+      <AuthProvider>
+        <AppRoutes/>
+        {/* Floating Dev Console */}
+            <FloatingDevConsole
+              allRestaurants={allRestaurants}
+              featuredRestaurants={featuredRestaurants}
+              loading={loading}
+              availableVersions={availableVersions}
+              selectedVersions={selectedVersions}
+              cuisineLength={CUISINES.length}
+            />
+      </AuthProvider>
+    // </ErrorBoundary>
   )
 }
 
